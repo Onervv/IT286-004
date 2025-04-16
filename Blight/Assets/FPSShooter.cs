3188b11dfdcb82908ef5baa52a51c397a4f374e3
MichaelPerrette <mcp62@njit.edu>
2025-04-16 15:30:25 -0400

    Projectiles, destroy on impact, no vfx explosion yet, halfway point

---
+++ Blight/Assets/FPSShooter.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class FPSShooter : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject projectile;
+    public Transform LHFirePoint, RHFirePoint;
+    
+    private Vector3 destination;
+    private bool leftHand;
+    private float projectileSpeed = 30;
+    public float arcRange = 1;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shootProjectile();
+        }
+    }
+
+    void shootProjectile()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            destination = hit.point;
+        }
+        else
+        {
+            destination = ray.GetPoint(1000);
+        }
+
+        if (leftHand)
+        {
+            leftHand = false;
+            InstantiateProjectile(LHFirePoint);
+        }
+        else
+        {
+            leftHand = true;
+            InstantiateProjectile(RHFirePoint);
+        }
+    }
+
+    void InstantiateProjectile(Transform firePoint)
+    {
+       var projectileObj = Instantiate(projectile, firePoint.position, Quaternion.identity) as GameObject; 
+       projectileObj.GetComponent<Rigidbody>().velocity = (destination - firePoint.position).normalized * projectileSpeed;
+       
+       iTween.PunchPosition(projectileObj, new Vector3 (Random.Range(-arcRange, arcRange), Random.Range(-arcRange, arcRange), 0), Random.Range(0.5f, 2));
+    }
+}
