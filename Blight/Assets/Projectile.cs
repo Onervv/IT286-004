2f10f27d02a609dd5677d94152695daafdc965e8
MichaelPerrette <mcp62@njit.edu>
2025-04-16 18:58:56 -0400

    Refactoring changes, new global volume, removed old maps

--- Blight/Assets/Projectile.cs
+++ Blight/Assets/Projectile.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
-{ 
+{
+    public GameObject impactVFX;
     private bool collided;
     void OnCollisionEnter(Collision collision)
     {
@@ -11,6 +12,11 @@ public class Projectile : MonoBehaviour
         if (collision.gameObject.tag != "Bullet" && collision.gameObject.tag != "Player" && !collided)
         {
             collided = true;
+            
+            var impact = Instantiate(impactVFX, collision.contacts[0].point, Quaternion.identity) as GameObject;
+            
+            Destroy(impact, 2f);
+            
             Destroy(gameObject);
         }
 
